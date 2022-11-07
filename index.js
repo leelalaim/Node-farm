8c84db9011360c649222d969472742236c5e3e7b
Laima <leelalaim@gmail.com>
2022-11-07 13:27:50 +0100

    add routing

---
+++ index.js
@@ -0,0 +1,59 @@
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+
+//////////////////////////////////
+// FILES
+
+//Blocking, synchronous way
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
+// const textOut = `This is what we know about avocado ${textIn}.\nCreatedon ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
+
+// console.log("File written!");
+
+//Non-blocking, asynchronous way
+
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   if (err) return console.log("ERROR!");
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
+//     fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
+//       fs.writeFile("./txt/final.txt", `${data2}.\n${data3}`, "utf8", (err) => {
+//         console.log("File has been written...");
+//       });
+//     });
+//   });
+// });
+// console.log("Reading...");
+
+//////////////////////////////////
+// SERVER
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObject = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+
+  const pathName = req.url;
+
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the OVERVIEW");
+  } else if (pathName === "/product") {
+    res.end("This is the PRODUCT");
+  } else if (pathName === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+    });
+    res.end("<h1>Page not found!</h1>");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to request on port 8000");
+});
